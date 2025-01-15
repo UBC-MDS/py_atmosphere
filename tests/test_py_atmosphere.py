83ca5ab9d12ba7f59bd8a7794b164f091749ac36
Francisco Ramirez <fraramfra@gmail.com>
2025-01-14 20:49:49 -0800

    Added feedback from previous PR from ZJ

--- tests/test_py_atmosphere.py
+++ tests/test_py_atmosphere.py
@@ -1,3 +1,4 @@
+import pytest
 from py_atmosphere.py_atmosphere import py_atmosphere
 import math
 
@@ -5,13 +6,37 @@ baseline_output_0m = [0.0, 15.04, 101.29]
 baseline_output_11000m = [11000.0, -56.35, 22.7349]
 baseline_output_25000m = [25000.0, -56.46, 2.52227]
 
-#Test input type
+#Valid Input Data Type Testing
 
-#Test plot
+def test_invalid_altitude_type_error():
+    """Test error handling when altitude is not a numeric value."""
+    with pytest.raises(TypeError, match="Altitude must be a numeric value."):
+        py_atmosphere("0.0", "m")
+
+def test_invalid_altitude_value_error():
+    """Test error handling when altitude is a negative value."""
+    with pytest.raises(ValueError, match="Altitude must be a non-negative."):
+        py_atmosphere(-1.0, "m")
+
+#Valid Output Testing
 
 def test_output_datatype():
     "Test output data type"
     assert isinstance(py_atmosphere(0.0, "m"), list)
+
+@pytest.mark.parametrize("altitude, alt_units", [
+    (0.0, "m"),  
+    (11000.0, "m"),  
+    (25000.0, "m"),  
+])
+def test_output_type_positive(altitude, alt_units):
+    """Test output data type"""
+    result = py_atmosphere(altitude, alt_units)
+    assert isinstance(result[0], float), "Altitude must be a float."
+    assert isinstance(result[1], float), "Temperature must be a float."
+    assert isinstance(result[2], float), "Pressure must be a float."
+
+#Valid Internal Unit Conversion
 
 def test_alt_unit_conversion():
     """Test altitude output conversion"""
@@ -32,6 +57,8 @@ def test_pressure():
     assert math.isclose(py_atmosphere(0, "m")[2], baseline_output_0m[2], abs_tol = 0.001), "The baseline output at 0 m is incorrect!"
     assert math.isclose(py_atmosphere(11000, "m")[2], baseline_output_11000m[2], abs_tol = 0.001), "The baseline output at 11000 m is incorrect!"
     assert math.isclose(py_atmosphere(25000, "m")[2], baseline_output_25000m[2], abs_tol = 0.001), "The baseline output at 25000 m is incorrect!"
+
+#Valid Input Altitude (Units and Range)
 
 def test_invalid_alt_units():
     """Test Error Message if Alt Units are not correct"""
